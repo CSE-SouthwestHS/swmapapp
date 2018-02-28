aad2483e108a556e05e652123ff99423c7fd6b29
Mark Pekala <mpek66@gmail.com>
2018-02-28 00:56:16 -0600

    Working Map

--- Draw.py
+++ Draw.py
@@ -12,11 +12,11 @@ def draw_vertex(draw, vertex, r):
 def draw_line(draw, v1, v2, r):
     draw_vertex(draw, v1, r)
     draw.line([v1.getx(), v1.gety(), v2.getx(), v2.gety()], \
-                             width = r, fill = (89, 79, 161))
+                             width = r*2, fill = (89, 79, 161))
     draw_vertex(draw, v2, r)
 
 #creates path images in a folder, in a folder
-def draw_path(vertices):
+def draw_path(vertices, r = 16):
     if len(vertices) < 1:
         #failsafe
         return False
@@ -31,7 +31,7 @@ def draw_path(vertices):
     curdraw = ImageDraw.Draw(curimage)
     if len(vertices) == 1:
         #one item case
-        draw_vertex(curdraw, vertices[0], 25)
+        draw_vertex(curdraw, vertices[0], r/2)
         name = "Nodes/" + vertices[0].getkey() + ".png"
         curimage.save(name)
         return True
@@ -39,22 +39,22 @@ def draw_path(vertices):
     pathfolder = "Paths/" + vertices[0].getkey() + "_" + vertices[-1].getkey() + "/"
     if not os.path.exists(pathfolder):
         os.makedirs(pathfolder)
-    draw_vertex(curdraw, vertices[0])
+    draw_vertex(curdraw, vertices[0], r/2)
     ix = 0
     while ix < len(vertices)-1:
         v1 = vertices[ix]
         v2 = vertices[ix + 1]
         if v1.getfloor() == v2.getfloor():
-            draw_line(curdraw, v1, v2, 25)
+            draw_line(curdraw, v1, v2, r)
         else:
-            draw_vertex(curdraw, v1)
+            draw_vertex(curdraw, v1, r/2)
             name = pathfolder + v1.getfloor() + ".png"
             curimage.save(name)
             curimage = Image.open(floor_map[v2.getfloor()])
             curdraw = ImageDraw.Draw(curimage)
-            draw_vertex(curdraw, v2)
+            draw_vertex(curdraw, v2, r/2)
         ix += 1
-    draw_vertex(curdraw, vertices[-1])
+    draw_vertex(curdraw, vertices[-1], r/2)
     name = pathfolder + vertices[-1].getfloor() + ".png"
     curimage.save(name)
     return True
